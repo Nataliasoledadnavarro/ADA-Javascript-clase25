438f6c8f081aa0d075545934be9a9f0c3bdb0e05
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-06-29 21:17:06 -0300

    Ejerccios propuestos en clase.

---
+++ index.js
@@ -0,0 +1,46 @@
+//Declarar la variable nombre con tu nombre
+
+let nombre = "Natalia";
+
+//Declarar la variable apellido con tu apellido
+
+let apellido = "Navarro";
+
+//Declarar año de nacimiento
+
+let añoDeNacimiento = 1985
+
+// Declarar la varibale edad con tu edad
+
+let edad = 36;
+
+//Declarar la variable dirección con tu dirección
+
+let direccion = "Julio a. Roca 2291";
+
+//Ver que tipo de dato es cada uno
+
+//Es variable
+
+//Mostrarlos en consola
+
+console.log(nombre);
+
+console.log(apellido)
+
+console.log(edad);
+
+console.log(direccion);
+
+//Hacer coperaciones con ellos. Concatenación, suma, etc.
+
+console.log(nombre+" "+apellido+" "+edad)
+
+console.log(2021-añoDeNacimiento)
+
+let nombreYEdad = nombre+" "+edad;
+
+console.log(nombreYEdad); 
+
+
+
